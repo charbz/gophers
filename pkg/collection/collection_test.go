fb7b65722d0cf6085a2bfb69bb0b11905e42eae7
Charbel <charbzg@gmail.com>
2024-11-11 02:01:17 -0800

    convert collection to an interface, created Sequence and ComparableSequence types that implement Collection (#4)
    Co-authored-by: Charb <charb@Charbs-MacBook-Pro.local>

---
+++ pkg/collection/collection_test.go
@@ -0,0 +1,51 @@
+package collection
+
+import (
+	"iter"
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// MockCollection implements the Collection interface for testing purposes
+type MockCollection[T any] struct {
+	items []T
+}
+
+func NewMockCollection[T any](items []T) *MockCollection[T] {
+	return &MockCollection[T]{items}
+}
+
+func (m *MockCollection[T]) All() iter.Seq2[int, T] {
+	return slices.All(m.items)
+}
+
+func (m *MockCollection[T]) Values() iter.Seq[T] {
+	return slices.Values(m.items)
+}
+
+func (m *MockCollection[T]) Length() int {
+	return len(m.items)
+}
+
+func (m *MockCollection[T]) Append(item T) {
+	m.items = append(m.items, item)
+}
+
+func (m *MockCollection[T]) ToSlice() []T {
+	return m.items
+}
+
+func (m *MockCollection[T]) New(s ...[]T) Collection[T] {
+	mock := &MockCollection[T]{}
+	if len(s) > 0 {
+		mock.items = s[0]
+	}
+	return mock
+}
+
+func TestMockCollectionImplementsCollection(t *testing.T) {
+	var m Collection[string] = NewMockCollection([]string{"a", "b", "c"})
+	assert.Equal(t, 3, m.Length())
+}
